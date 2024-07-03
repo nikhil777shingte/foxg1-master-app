1c9882c5f308e2686b170eda904fb2e03d4d3b2a
nikhil777shingte <nikhil777shingte@gmail.com>
2024-07-02 23:10:00 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          FOXG1 Master App
         </a>
       </header>
     </div>
